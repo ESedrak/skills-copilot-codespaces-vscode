3fa8f61651e8d659cdbd5dd9d24039cb3e1f6623
ESedrak <100348676+ESedrak@users.noreply.github.com>
2024-06-21 06:14:19 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,30 +1,27 @@
-// Create Web Server
+// Create web server
+// Create an express app
+// Create a route for GET request to /comments
+// Respond with a JSON object with some comments
+// Start the server and listen on port 3000
+// Use curl to make a GET request to /comments
+// Make a POST request to /comments with a JSON body
+// Respond with a JSON object with a message
+// Respond with a JSON object with the data from the request
+// Respond with a JSON object with the data from the request
+// Respond with a JSON object with the data from the request
 
-// Import modules
 const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-
-// Create Web Server
 const app = express();
-app.use(bodyParser.json());
+const port = 3000;
 
-// Load comments
-let comments = JSON.parse(fs.readFileSync('./comments.json'));
-
-// Define routes
 app.get('/comments', (req, res) => {
-  res.send(comments);
+    res.json({comments: ["This is a comment", "This is a comment", "This is a comment"]});
 });
 
 app.post('/comments', (req, res) => {
-  const comment = req.body;
-  comments.push(comment);
-  fs.writeFileSync('./comments.json', JSON.stringify(comments));
-  res.send({ message: 'Comment added' });
+    res.json({message: "This is a POST request"});
 });
 
-// Start Web Server
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
